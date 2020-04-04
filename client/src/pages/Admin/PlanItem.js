de5d3c2662d29b724e1c15f3c0c534aa9a1b8297
nhduy <nhduy220597@gmail.com>
2020-04-04 10:09:11 +0700

    test add object to array

--- client/src/pages/Admin/PlanItem.js
+++ client/src/pages/Admin/PlanItem.js
@@ -1,28 +1,33 @@
-import React, { Fragment } from 'react'
-import PropTypes from 'prop-types'
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 
-const PlanItem = ({user, index}) => {
-    return (
-        <Fragment>
-            <tr>
-                <th scope='row'>{index + 1}</th>
-                <td>{user._id}</td>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
-                <td>Phong Dien - Can Tho</td>
-                <td>
-                    <a href='#/'>
-                        <i className='fa fa-pencil' aria-hidden='true' />
-                        Phân công
-                    </a>
-                </td>
-            </tr>
-        </Fragment>
-    )
-}
+const PlanItem = ({ user, index }) => {
+  return (
+    <Fragment>
+      <tr>
+        <th scope='row'>{index + 1}</th>
+        <td>{user._id}</td>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        <td>Phong Dien - Can Tho</td>
+        <td>
+          <button onClick={(e) => onClick(user.name)} value=' Phân công'>
+            <i className='fa fa-pencil' aria-hidden='true' />
+          </button>
+        </td>
+      </tr>
+    </Fragment>
+  );
+};
+
+const users = [];
+const onClick = (e) => {
+  users.push(e);
+  console.log(users);
+};
 
 PlanItem.propTypes = {
-    user: PropTypes.object.isRequired
-}
+  user: PropTypes.object.isRequired,
+};
 
-export default PlanItem
+export default PlanItem;
--- client/src/pages/WareHouse/WareHouse.js
+++ client/src/pages/WareHouse/WareHouse.js
@@ -1,49 +1,46 @@
-import React, { Fragment, useEffect } from 'react'
-import ProductItem from './ProductItem'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import Spinner from './../../components/Spinner/Spinner'
-import {getAllProduct} from './../../actions/product'
+import React, { Fragment, useEffect } from 'react';
+import ProductItem from './ProductItem';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from './../../components/Spinner/Spinner';
+import { getAllProduct } from './../../actions/product';
 
-const WareHouse = ({getAllProduct, product: {loading, products}}) => {
-    useEffect(() => {
-    getAllProduct()
-    }, [getAllProduct])
-    return loading ? (<Spinner/>) :
-    (
-        <Fragment>
-            
-        
-            <table className="table mt-5 container">
-                <thead className="thead-light">
-                    <tr>
-                        <th scope="col">STT</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Quantity</th>
-                        <th scope="col">Price</th>
-                        <th scope="col">Manufacturer</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        products.map((val, index) => {
-                            return <ProductItem key={index} index={index} product={val}/>
-                        })
-                    }
-                    
-                </tbody>
-            </table>
-        </Fragment>
-    )
-}
-const mapStateToProps = state => {
-    return {
-        product: state.product
-    }
-}
+const WareHouse = ({ getAllProduct, product: { loading, products } }) => {
+  useEffect(() => {
+    getAllProduct();
+  }, [getAllProduct]);
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <table className='table mt-5 container'>
+        <thead className='thead-light'>
+          <tr>
+            <th scope='col'>STT</th>
+            <th scope='col'>Name</th>
+            <th scope='col'>Quantity</th>
+            <th scope='col'>Price</th>
+            <th scope='col'>Manufacturer</th>
+            <th scope='col'>Add</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((val, index) => {
+            return <ProductItem key={index} index={index} product={val} />;
+          })}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+const mapStateToProps = (state) => {
+  return {
+    product: state.product,
+  };
+};
 WareHouse.propTypes = {
-    getAllProduct: PropTypes.func.isRequired,
-    product: PropTypes.object.isRequired
-}
+  getAllProduct: PropTypes.func.isRequired,
+  product: PropTypes.object.isRequired,
+};
 
-export default connect(mapStateToProps, {getAllProduct})(WareHouse)
+export default connect(mapStateToProps, { getAllProduct })(WareHouse);
