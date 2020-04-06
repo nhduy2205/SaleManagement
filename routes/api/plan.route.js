1b0305fe1306bbd3237cc55ccf2568d013df4194
nguyendong98 <namasd2001@gmail.com>
2020-04-06 12:10:54 +0700

    fixed date route plan

--- routes/api/plan.route.js
+++ routes/api/plan.route.js
@@ -4,7 +4,7 @@ const auth = require('./../../middleware/auth')
 const admin = require('./../../middleware/admin')
 const Plan = require('./../../models/Plan')
 const User = require('./../../models/User')
-
+const moment = require('moment')
 
 // @route   DELETE api/plans/:id
 // @desc    route post plan
@@ -15,27 +15,44 @@ router.post('/:id', auth, admin, async (req, res) => {
 
     try {
         const dateNow = new Date()
-        const date = dateNow.getDate()
-        const month = dateNow.getMonth()
-        const year = dateNow.getFullYear()
+        const today = moment(dateNow).format('YYYY-MM-DD')
         const user = await User.findById(req.params.id)
-        var existPlan = await Plan.findOne({user: req.params.id})
-        if(existPlan 
-            && existPlan.date.getDate() === date
-            && existPlan.date.getMonth() === month
-            && existPlan.date.getFullYear() === year
-            ){
+        var allplan = await Plan.find({user : req.params.id})
+        const existplan = allplan.map(val => {
+            if(moment(val.date).format('YYYY-MM-DD') === today){
+                return val
+            }
+        })
+        console.log(existplan)
+        var count = 0;
+        existplan.map(val => count++)
+        if(count !== 0){
             return res.status(400).json({ errors: [{ msg: 'Plan\'s user today is already exists' }] })
         }
+        // const date = dateNow.getDate()
+        // const month = dateNow.getMonth()
+        // const year = dateNow.getFullYear()
+        // const user = await User.findById(req.params.id)
+        // var existPlan = await Plan.findOne({user: req.params.id})
+        // if(existPlan 
+        //     && existPlan.date.getDate() === date
+        //     && existPlan.date.getMonth() === month
+        //     && existPlan.date.getFullYear() === year
+        //     ){
+        //     return res.status(400).json({ errors: [{ msg: 'Plan\'s user today is already exists' }] })
+        // }
+        else {
+            var newPlan = new Plan({
+                user: user,
+                product_list: req.body,
+                
+                      
+            })
+            await newPlan.save()
+            return res.status(200).json(newPlan)
+        }
         
-        var newPlan = new Plan({
-            user: user,
-            product_list: req.body,
-            
-                  
-        })
-        await newPlan.save()
-        return res.status(200).json(newPlan)
+        
     } catch (error) {
         console.error(error.message)
         return res.status(500).json('Server Error!')
@@ -49,7 +66,7 @@ router.post('/:id', auth, admin, async (req, res) => {
 //Xem kế hoạch
 router.get('/', auth, admin, async (req, res) => {
     try {
-        const plans = await Plan.find()
+        const plans = await Plan.find().sort({date: -1})
         return res.status(200).json(plans)    
     } catch (error) {
         console.error(error.message)
