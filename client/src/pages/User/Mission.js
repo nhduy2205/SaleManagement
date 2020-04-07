34c4c50d3c47160145b84f48a34f9bfe6259fa0a
nhduy <nhduy220597@gmail.com>
2020-04-07 12:31:39 +0700

    Css mission page, complete function

--- client/src/pages/User/Mission.js
+++ client/src/pages/User/Mission.js
@@ -9,7 +9,7 @@ const Mission = ({ getUserMission, plan: { mission, loading } }) => {
     getUserMission();
   }, [getUserMission]);
 
-  console.log(mission);
+  // console.log(today.now());
 
   return loading && mission === null ? (
     <Spinner />
@@ -26,18 +26,26 @@ const Mission = ({ getUserMission, plan: { mission, loading } }) => {
         <span className='admin__menu-home'>Mission of the day</span>
       </div>
       <Fragment>
-        <table>
-          <tbody>{showMission(mission.product_list)}</tbody>
+        <table className='table mt-5 container'>
+          <thead className='thead-light'>
+            <tr>
+              <th scope='col'>STT</th>
+              <th scope='col'>Name</th>
+              <th scope='col'>Quantity</th>
+              <th scope='col'>Price</th>
+              <th scope='col'>Sale price</th>
+            </tr>
+          </thead>
+          <tbody>{showMission(mission)}</tbody>
         </table>
       </Fragment>
     </section>
   );
 };
 
-const showMission = (e) => {
-  if (e != null) {
-    e.map((value, index) => {
-      console.log(value.product);
+const showMission = (mission) => {
+  if (mission !== null) {
+    return mission.product_list.map((value, index) => {
       return (
         <tr key={index}>
           <th scope='row'>{index + 1}</th>
@@ -46,10 +54,18 @@ const showMission = (e) => {
           </td>
           <td>{value.quantity}</td>
           <td>{value.price} $</td>
+          <td>{value.price + value.price * 0.1} $</td>
         </tr>
       );
     });
-  } else return <tr>Chưa được phân công</tr>;
+  } else
+    return (
+      <tr>
+        <td>
+          <b>No plan to day</b>
+        </td>
+      </tr>
+    );
 };
 
 Mission.propTypes = {
