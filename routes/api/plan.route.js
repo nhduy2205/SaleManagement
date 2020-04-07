74414f4c1379942ae29171cc84790140ac734f5c
nguyendong98 <namasd2001@gmail.com>
2020-04-07 09:11:24 +0700

    fixed api create plan

--- routes/api/plan.route.js
+++ routes/api/plan.route.js
@@ -14,20 +14,26 @@ router.post('/:id', auth, admin, async (req, res) => {
     // const { product_list, date_now } = req.body 
 
     try {
-        const dateNow = new Date()
-        const today = moment(dateNow).format('YYYY-MM-DD')
-        const user = await User.findById(req.params.id)
+        
+        
+        
         var allplan = await Plan.find({user : req.params.id})
+        var count = 0;
         const existplan = allplan.map(val => {
+            
+            var dateNow = new Date()
+            var today = moment(dateNow).format('YYYY-MM-DD')
+            console.log(today)
+            console.log(moment(val.date).format('YYYY-MM-DD'))
             if(moment(val.date).format('YYYY-MM-DD') === today){
-                console.log(moment(val.date).format('YYYY-MM-DD'))
-                console.log(today)
-                return val
+                // console.log(moment(val.date).format('YYYY-MM-DD'))
+                // console.log(val)
+                count ++
+                return count
             }
         })
-        console.log(existplan)
-        var count = 0;
-        existplan.map(val => count++)
+        console.log(count)
+        // existplan.map(val => count++)
         // console.log(count)
         if(count === 1 ){
             return res.status(400).json({ errors: [{ msg: 'Plan\'s user today is already exists' }] })
@@ -44,16 +50,18 @@ router.post('/:id', auth, admin, async (req, res) => {
         //     ){
         //     return res.status(400).json({ errors: [{ msg: 'Plan\'s user today is already exists' }] })
         // }
-        else {
-            var newPlan = new Plan({
-                user: user,
-                product_list: req.body,
-                
-                      
-            })
-            await newPlan.save()
-            return res.status(200).json(count)
-        }
+        
+        var user = await User.findById(req.params.id)
+        console.log(user)
+        var newPlan = new Plan({
+            user: user,
+            product_list: req.body,
+            
+                    
+        })
+        await newPlan.save()
+        return res.status(200).json(newPlan)
+    
         
         
     } catch (error) {
@@ -70,6 +78,8 @@ router.post('/:id', auth, admin, async (req, res) => {
 router.get('/', auth, admin, async (req, res) => {
     try {
         const plans = await Plan.find().sort({date: -1})
+        // console.log(moment('2020-04-06T15:14:42.632Z').format('YYYY-MM-DD'))
+       
         return res.status(200).json(plans)    
     } catch (error) {
         console.error(error.message)
